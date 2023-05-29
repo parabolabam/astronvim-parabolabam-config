0191396ef538f3d486dc6435e7ec1dbb8cee7f44
Vladislav Sorokin <parabolabam@192.168.1.127>
2023-05-29 23:10:51 +0300

    remove duplicates, change color scheme to more nice ont

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "catppuccin-frappe",
+  colorscheme = "everforest",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
