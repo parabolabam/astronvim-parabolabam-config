aa442b7660cd92910c15b46f25a843225a9944cd
Vladislav Sorokin <parabolabam@192.168.1.127>
2023-06-12 22:43:44 +0300

    remove telescope theming

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -2,11 +2,6 @@ return {
   "nvim-telescope/telescope.nvim",
   opts = function()
     return {
-      pickers = {
-        find_files = {
-          theme = "dropdown",
-        }
-      }
     }
   end,
 }
